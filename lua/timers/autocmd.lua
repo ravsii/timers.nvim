0553759e053bac318385894eadf807cd9504bbff
Alex <naplle58@gmail.com>
2025-09-15 21:34:19 +0300

    fix(manager): error on cancel

--- lua/timers/autocmd.lua
+++ lua/timers/autocmd.lua
@@ -1,10 +1,15 @@
+local manager = require("timers.manager")
+
 local M = {}
 
-local aug = vim.api.nvim_create_augroup("TimerSaveState", { clear = true })
+local aug = vim.api.nvim_create_augroup("timers.nvim/autocmd", { clear = true })
+
 function M.setup()
   vim.api.nvim_create_autocmd("VimLeavePre", {
     group = aug,
-    callback = function() M.save_state() end,
+    callback = function()
+      manager.save_state()
+    end,
   })
 end
 
